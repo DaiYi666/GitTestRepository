9be0cfa825cd81622d0b6ead444e689c453c145b
DaiYi <484201132@qq.com>
2020-09-28 18:02:32 +0800

    other

--- spring-ioc-xml/src/main/java/com/itxh/ApplicationContext.java
+++ spring-ioc-xml/src/main/java/com/itxh/ApplicationContext.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationContext {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        //初始化容器
         context.close();
     }
 }
